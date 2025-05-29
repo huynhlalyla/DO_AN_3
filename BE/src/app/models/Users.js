8cca86eecde4a444aa49b2ea31d8b187b6375243
Haryu2311 <kakasa901@gmail.com>
2025-05-29 19:47:26 +0700

    backend

--- BE/src/app/models/Users.js
+++ BE/src/app/models/Users.js
@@ -1,42 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const crypto = require('crypto');
 
 const User = new Schema({
     name: {type: String, required: true},
     password: {type: String, required: true},
     email: {type: String, required: true},
     phone: {type: String, required: true},
-    created_budgets: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Budget',
-            default: []
-        }
-    ],
-    created_transactions: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Transaction',
-            default: []
-        }
-    ],
-    created_categories: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Category',
-            default: []
-        }
-    ],
-    notifies: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Notify',
-            default: []
-        }
-    ],
-    resetPasswordToken: {type: String, default: null},
-    resetPasswordExpires: {type: Date, default: null},
 }, {
     timestamps: true
 });
--- BE/src/router/user.js
+++ BE/src/router/user.js
@@ -5,16 +5,7 @@ const userController = require('../app/controllers/userController');
 
 router.get('/add', userController.addUser);
 router.post('/authenticate', userController.authenticateUser);
-router.post('/check', userController.checkUser);
 router.post('/register',userController.registerUser);
 router.post('/login', userController.loginUser);
-router.put('/update', userController.updateUser);
-router.put('/change-password', userController.changePassword);
-router.post('/get-user', userController.getUser);
-//create token reset pass
-router.post('/create-token', userController.createTokenResetpassword);
-router.post('/verify-token', userController.verifyToken);
-router.post('/reset-password', userController.resetPassword);
-
 
 module.exports = router;
